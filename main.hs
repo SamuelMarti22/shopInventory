1a3f46cc36fda2b77109e168660d26b866958057
SamuelMarti22 <samuelmartinezarteaga22@gmail.com>
2024-08-20 22:08:27 -0500

    .

--- main.hs
+++ main.hs
@@ -1,3 +1,11 @@
 main :: IO ()
 main = do
     putStrLn "Hola Mundo"
+    while (true){
+        putStrLn "¿Qué deseas hacer?"
+        putStrLn "1. Ingresar artículo"
+        putStrLn "2. Buscar artículo por categoría"
+        putStrLn "3. Listar todos los artícutlos"
+        putStrLn "4. Mostrar cantidad de artículos por categorías"
+        putStrLn "5. Salir"
+    }
