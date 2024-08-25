3292f2f40e3b2c2ff9eb046ea91a89252942e1ce
SamuelMarti22 <samuelmartinezarteaga22@gmail.com>
2024-08-24 22:50:43 -0500

    Programita terminao
    Co-authored-by: lacastrilf <lacastrilf@users.noreply.github.com>

--- main.hs
+++ main.hs
@@ -3,10 +3,18 @@ import Data.List
 import System.IO
 import Control.Exception
 import Control.DeepSeq (deepseq)
+import Data.Maybe (fromJust, isJust)
 
 -- Objeto para representar la información de los Articulos
 data Articulo = Articulo { nombre :: String, categoria :: String, stock :: Int }
     deriving (Show, Read)
+
+-- Función para guardar la información de los vehículos en un archivo de texto
+guardarInventario :: [Articulo] -> IO ()
+guardarInventario inventario = do
+    withFile "inventario.txt" WriteMode $ \h -> do
+        hPutStr h (unlines (map mostrarInventario inventario))
+    putStrLn "Inventario guardado en el archivo inventario.txt."
 
 -- Función cargar inventario
 cargarInventario :: IO [Articulo]
@@ -27,7 +35,7 @@ cargarInventario = do
 -- Función Mostrar inventario
 mostrarInventario :: Articulo -> String
 mostrarInventario (Articulo nombre categoria stock) =
-    "Articulo: nombre = \"" ++ nombre ++ "\", categoria = \"" ++ categoria ++ "\", stock = " ++ show stock ++ ""
+    "Articulo {nombre = \"" ++ nombre ++ "\", categoria = \"" ++ categoria ++ "\", stock = " ++ show stock ++ "}"
 
 -- Función para listar los artículos en el inventario
 listarInventario :: [Articulo] -> IO ()
@@ -35,6 +43,35 @@ listarInventario [] = putStrLn "No hay artículos en el inventario."
 listarInventario articulos = do
     putStrLn "Artículos en el inventario:"
     mapM_ (putStrLn . mostrarInventario) articulos
+
+-- Función para buscar un articulo
+buscarArticulo :: String -> [Articulo] -> Maybe Articulo
+buscarArticulo articuloInventario inventario =
+    find (\v -> articuloInventario == nombre v) inventario
+
+
+-- Función para buscar una categoría
+eliminarArticulo :: String -> [Articulo] -> [Articulo]
+eliminarArticulo nombreArticulo inventario =
+    filter (\v -> nombre v /= nombreArticulo) inventario
+    
+-- Función para buscar una categoría
+buscarCategoria :: String -> [Articulo] -> [Articulo]
+buscarCategoria categoriaInventario inventario =
+    filter (\v -> categoriaInventario == categoria v) inventario
+
+ -- Función para buscar e imprimir un Articulo si lo encuentra
+buscarYImprimirArticulo :: String -> [Articulo] -> Bool -> IO ()
+buscarYImprimirArticulo categoriaInventario inventario estado = do
+    let resultados = buscarCategoria categoriaInventario inventario
+    let contador = length resultados
+    if null resultados
+        then putStrLn "No se encontraron artículos."
+        else do
+            putStrLn $ "Se encontraron: "++show contador ++" artículos en la categoria "++ categoriaInventario
+            if estado
+                then mapM_ print resultados
+                else putStrLn ""
 
 -- Función principal del ciclo
 cicloPrincipal :: [Articulo] -> IO ()
@@ -50,27 +87,58 @@ cicloPrincipal inventario = do
 
     case opcion of
         "1" -> do
-            putStrLn "\nIngresar artículo"
-            putStrLn "Ingresar articulo nuevo"
-            putStrLn "Ingresar artículo existente"
+            putStrLn "\nIngresar artículo:"
+            putStrLn "1. Ingresar articulo nuevo"
+            putStrLn "2. Ingresar artículo existente"
 
             opcionIngresar <- getLine
 
             case opcionIngresar of
                 "1" -> do
-                putStrLn "Ingresar nombre del articulo nuevo"
-                nombre <- getLine
-                putStrLn "Ingresar categoria del articulo nuevo"
-                categoria <- getLine
-                putStrLn "Ingresar stock del articulo nuevo"
-                stock <- readLn
-                 cicloPrincipal inventario
-                
-
-            cicloPrincipal inventario
+                    putStrLn "Ingresar nombre del articulo nuevo"
+                    nombre <- getLine
+                    let articulo = buscarArticulo nombre inventario
+                    if isJust articulo
+                        then do 
+                            putStrLn "El articulo ya existe"
+                            cicloPrincipal inventario
+                        else do
+                        putStrLn "Ingresar categoria del articulo nuevo"
+                        categoria <- getLine
+                        putStrLn "Ingresar stock del articulo nuevo"
+                        stock <- readLn :: IO Int
+                        let nuevoArticulo = Articulo nombre categoria stock
+                        let inventarioActualizado = nuevoArticulo : inventario
+                        guardarInventario inventarioActualizado
+                        cicloPrincipal inventarioActualizado
+                "2" -> do
+                    putStrLn "Ingresar nombre del articulo existente"
+                    nombre <- getLine
+                    let articuloExistente = buscarArticulo nombre inventario
+                    putStrLn "Ingresar cuánto de este artículo quieres agregar al sistema"
+                    stockN <- readLn :: IO Int
+                    if isJust articuloExistente
+                        then do
+                            let articulo = fromJust articuloExistente
+                            let stockActual = stock articulo
+                            let stockNuevo = stockActual + stockN
+                            let articuloActualizado = articulo { stock = stockNuevo }
+                            let inventarioSinArticulo = eliminarArticulo nombre inventario
+                            let inventarioActualizado = articuloActualizado : inventarioSinArticulo
+                            guardarInventario inventarioActualizado
+                            cicloPrincipal inventarioActualizado
+                        else do
+                            putStrLn "El artículo no existe en el inventario."
+                            cicloPrincipal inventario
+                _ -> do
+                    putStrLn "Opción no válida"
+                    cicloPrincipal inventario
         
         "2" -> do
             putStrLn "Buscar artículo por categoría"
+            putStrLn "Ingresar categoria del articulo a buscar"
+            categoria <- getLine
+            buscarYImprimirArticulo categoria inventario True
             cicloPrincipal inventario
         
         "3" -> do
@@ -80,6 +148,9 @@ cicloPrincipal inventario = do
         
         "4" -> do
             putStrLn "Mostrar cantidad de artículos por categorías"
+            putStrLn "Ingresar categoria del articulo a buscar"
+            categoria <- getLine
+            buscarYImprimirArticulo categoria inventario False
             cicloPrincipal inventario
         
         "5" -> putStrLn "Saliendo del programa..."
